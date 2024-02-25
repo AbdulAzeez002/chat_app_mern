194f451565c6d91d0e760e14f96e146c9527e800
AbdulAzeez002 <azeeztp2020@gmail.com>
2024-02-25 15:48:28 +0530

    added initial ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Login from './pages/login/Login';
+import Signup from './pages/signup/Signup';
+import Home from './pages/home/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className='p-4 h-screen flex items-center justify-center'>
+        <Home/>
     </div>
+    
   );
 }
 
